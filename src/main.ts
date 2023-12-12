c2cfb60d18c308292e0e582ac6669446f9790a57
emil <emil.doychinov@zettahost.com>
2023-12-12 14:04:05 +0200

    use port from .env

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'
-
-
+import { Inject, ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config';
 const expressApp = require('express')();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), { cors: true });
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('DEV_PORT');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(5000);
+  await app.listen(port as number);
 }
 bootstrap();
